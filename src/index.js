e0b2e8df466ef6c1c48e25262b84d1c042922f50
Roman Lagunov <roman.lagunov@anaplan.com>
2018-10-18 14:34:25 -0700

    Tried to fix IE and MS Edge performance issues

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ export default {
 
         browser.get(pageUrl);
         this.openedBrowsers[id] = browser;
-        //browser.manage().timeouts().implicitlyWait(80000);
+        browser.manage().timeouts().implicitlyWait(80000);
         browser.sleep(600000);
     },
 
